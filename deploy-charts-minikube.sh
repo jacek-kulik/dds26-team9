1e4d163709ace1f7a6e9ea18d17341ccdd71fd7e
Jacek Kulik <macienias@gmail.com>
2026-03-08 14:23:24 +0100

    Add redis pod creation

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -4,3 +4,5 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+REDIS_PASSWORD=$(kubectl get secret --namespace default redis -o jsonpath="{.data.redis-password}" | base64 -d)
+kubectl run --namespace default redis-client --restart='Never'  --env REDIS_PASSWORD="$REDIS_PASSWORD"  --image registry-1.docker.io/bitnami/redis:latest --command -- sleep infinity
